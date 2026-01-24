df00c42b7445eef4915028b0fcbb227494211563
xodudgks <legoherohty@gmail.com>
2026-01-24 17:47:15 +0900

    Day 9 : State로 상태관리하기

--- React/section 05/src/App.jsx
+++ React/section 05/src/App.jsx
@@ -1,25 +1,71 @@
+// 5.1 ~ 5.5 실습 코드
+
+// import "./App.css";
+// import Header from "./components/Header";
+// import Main from "./components/Main";
+// import Footer from "./components/Footer";
+// import Button from "./components/Button";
+
+// function App() {
+//   const buttonProps = {
+//     text: "메일",
+//     color: "red",
+//     a: 1,
+//     b: 2,
+//     c: 3,
+//   };
+
+//   return (
+//     <>
+//       <Button {...buttonProps} />
+//       <Button text={"카페"} />
+//       <Button text={"블로그"}>
+//         <Header />
+//       </Button>
+//     </>
+//   );
+// }
+
+// export default App;
+
+// 5.6 이후 실습 코드
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
 
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <Header />
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === "ON" ? "OFF" : "ON");
+          }}
+        >
+          {light === "ON" ? "끄기" : "켜기"}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+
+        <button
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          -
+        </button>
+      </div>
     </>
   );
 }

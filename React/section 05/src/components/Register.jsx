e882ded10800e9f88f5603ea20d8992ae05c1132
xodudgks <legoherohty@gmail.com>
2026-01-24 19:03:37 +0900

    Day 9 : React 입문 (State로 사용자 입력 관리하기 2)

--- React/section 05/src/components/Register.jsx
+++ React/section 05/src/components/Register.jsx
@@ -7,37 +7,45 @@ import { useState } from "react";
 // 4. 자기소개
 
 const Register = () => {
-  const [name, setName] = useState("이름");
-  const [birth, setBirth] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("");
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
-  };
-
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  };
-
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
+  const onChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} placeholder={"이름"} />
+        <input
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
       </div>
       <div>
-        <input value={birth} onChange={onChangeBirth} type="date" />
+        <input
+          name="birth"
+          value={input.birth}
+          onChange={onChange}
+          type="date"
+        />
       </div>
       <div>
-        <select value={country} onChange={onChangeCountry} type="국적">
+        <select
+          name="country"
+          value={input.country}
+          onChange={onChange}
+          type="국적"
+        >
           <option></option>
           <option value="kr">한국</option>
           <option value="us">미국</option>
@@ -46,7 +54,12 @@ const Register = () => {
       </div>
 
       <div>
-        <textarea value={bio} onChange={onChangeBio} type="자기소개" />
+        <textarea
+          name="bio"
+          value={input.bio}
+          onChange={onChange}
+          type="자기소개"
+        />
       </div>
     </div>
   );

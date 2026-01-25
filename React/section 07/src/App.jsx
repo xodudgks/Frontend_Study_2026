321b15af8f72331621f392621f6db1c16010db7d
xodudgks <legoherohty@gmail.com>
2026-01-25 23:42:20 +0900

    Day 10 : 라이프사이클 (useEffect로 라이프사이클 제어하기)

--- React/section 07/src/App.jsx
+++ React/section 07/src/App.jsx
@@ -1,15 +1,34 @@
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
-import { useState, useEffect } from "react";
+import Even from "./components/Even";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
+  const isMount = useRef(false);
 
+  // 1. 마운트 : 탄생
   useEffect(() => {
-    console.log(`count: ${count} / input: ${input}`);
-  }, [count, input]);
+    console.log("mount");
+  }, []);
+
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  // 3. 언마운트 : 죽음
+
+  // 7.2강 (useEffect 도입)
+  // useEffect(() => {
+  //   console.log(`count: ${count} / input: ${input}`);
+  // }, [count, input]);
   // 의존성 배열 -> dependency array (deps)
 
   const onClickButton = (value) => {
@@ -29,6 +48,7 @@ function App() {
       </section>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />

c23d3a8cc5cd7498c814725c3a6e134ddff89202
xodudgks <legoherohty@gmail.com>
2026-01-28 21:17:18 +0900

    Day 12 : 프로젝트 2 (UI 구현하기)

--- React/section 08/src/App.jsx
+++ React/section 08/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import Editer from "./components/Editer";
+import Header from "./components/Header";
+import List from "./components/List";
+import TodoItem from "./components/TodoItem";
 
 function App() {
-  return <>TodoList</>;
+  return (
+    <div className="App">
+      <Header />
+      <Editer />
+      <List />
+    </div>
+  );
 }
 
 export default App;
---
+++ React/section 08/src/components/Editer.jsx
@@ -0,0 +1,12 @@
+import "./Editer.css";
+
+const Editer = () => {
+  return (
+    <div className="Editer">
+      <input placeholder="새로운 Todo..." />
+      <button>추가</button>
+    </div>
+  );
+};
+
+export default Editer;
